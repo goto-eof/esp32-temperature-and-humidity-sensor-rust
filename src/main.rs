6b3ee0723ed88ff3b4f37d875dc92f96653d2cc5
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-03 00:04:03 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use dht_sensor::dht11;
 use dht_sensor::DhtReading;
 use esp_idf_hal::delay;
+use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_sys as _;
 use log::{error, info};
-use std::thread;
-use std::time::Duration;
 fn main() {
     esp_idf_sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
@@ -15,7 +14,7 @@ fn main() {
     let pin: gpio::Gpio5 = peripherals.pins.gpio5;
     let mut sensor = gpio::PinDriver::input_output(pin).unwrap();
     sensor.set_high().unwrap();
-    thread::sleep(Duration::from_millis(1000));
+    FreeRtos::delay_ms(1000);
     let mut i: u64 = 1;
     loop {
         match dht11::Reading::read(&mut delay::Ets, &mut sensor) {
@@ -26,6 +25,6 @@ fn main() {
             Err(e) => error!("Failed to retrieve information: {:?}", e),
         }
         i = i + 1;
-        thread::sleep(Duration::from_secs(3));
+        FreeRtos::delay_ms(3000);
     }
 }
